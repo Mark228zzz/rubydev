a50694129924b634de8996b2f8390a263f59f7cb
Mark <mark.mazur007@gmail.com>
2021-06-22 11:34:09 +0300

    update tutorial_2

--- 1/tutorial_2.rb
+++ 1/tutorial_2.rb
@@ -13,3 +13,26 @@ p array
 puts '_____________________________'
 
 puts array
+
+p 1.class
+p "Hello".class
+p false.class
+p nil.class
+
+puts'_______________'
+
+p 1.to_f
+p 1.to_s
+p 1.to_i
+
+puts'_________________'
+
+p rand(1..10)
+
+p'random 1,2,3,4,5,6,7,8,9,10'
+
+puts'_________________________________'
+
+p rand(1...10)
+
+p'random 1,2,3,4,5,6,7,8,9'
