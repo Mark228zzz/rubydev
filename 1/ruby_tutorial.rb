da22e867f3fc9b401ecc440c8dd5010502f29b0b
Mark <mark.mazur007@gmail.com>
2021-06-22 11:16:06 +0300

    add tutorial_2

--- 1/ruby_tutorial.rb
+++ 1/ruby_tutorial.rb
@@ -1,20 +1,9 @@
 class RubyTutorial
 
   def self.description
-    a,b,c = 1,2,3
+    a,b,c = 6,2,3
     a ? b : c
-    for i  in 0..5
-      p i
-    end
-    for i in 0...5
-      p i
-    end
-    p '_____'
-
-    array = %w[sasha dasha pasha]
-    array.map { |i| p i }
   end
 end
-
 
 RubyTutorial.description
---
+++ 1/tutorial_2.rb
@@ -0,0 +1,15 @@
+for i in 0..5
+  p i
+end
+for i in 0...5
+  p i
+end
+p '_____'
+
+array = %w[sasha dasha pasha]
+
+p array
+
+puts '_____________________________'
+
+puts array
